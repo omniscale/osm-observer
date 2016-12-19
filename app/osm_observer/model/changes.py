8aee15743dd0c9376213482c0b9dd19fdada8970
Kai Culemann <culemann@omniscale.de>
2016-12-19 11:28:21 +0100

    Added table descriptions for changes nodes, ways and relations

--- app/osm_observer/model/changes.py
+++ app/osm_observer/model/changes.py
@@ -2,7 +2,7 @@ from osm_observer.extensions import db
 from geoalchemy2.types import Geometry
 from sqlalchemy.dialects.postgresql import HSTORE
 
-__all__ = ['changesets']
+__all__ = ['changesets', 'nodes', 'ways', 'relations']
 
 changesets = db.Table(
     'changesets', db.MetaData(),
@@ -16,4 +16,53 @@ changesets = db.Table(
     db.Column('user_id', db.Integer),
     db.Column('tags', HSTORE),
     schema='changes'
+)
+
+
+nodes = db.Table(
+    'nodes', db.MetaData(),
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('add', db.Boolean),
+    db.Column('modify', db.Boolean),
+    db.Column('delete', db.Boolean),
+    db.Column('geometry', Geometry(geometry_type='Point', srid=4326)),
+    db.Column('user_name', db.String),
+    db.Column('user_id', db.Integer),
+    db.Column('timestamp', db.DateTime),
+    db.Column('version', db.Integer),
+    db.Column('tags', HSTORE),
+    db.Column('changeset', db.Integer),
+    schema='changes',
+)
+
+
+ways = db.Table(
+    'ways', db.MetaData(),
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('add', db.Boolean),
+    db.Column('modify', db.Boolean),
+    db.Column('delete', db.Boolean),
+    db.Column('user_name', db.String),
+    db.Column('user_id', db.Integer),
+    db.Column('timestamp', db.DateTime),
+    db.Column('version', db.Integer),
+    db.Column('tags', HSTORE),
+    db.Column('changeset', db.Integer),
+    schema='changes',
+)
+
+
+relations = db.Table(
+    'relations', db.MetaData(),
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('add', db.Boolean),
+    db.Column('modify', db.Boolean),
+    db.Column('delete', db.Boolean),
+    db.Column('user_name', db.String),
+    db.Column('user_id', db.Integer),
+    db.Column('timestamp', db.DateTime),
+    db.Column('version', db.Integer),
+    db.Column('tags', HSTORE),
+    db.Column('changeset', db.Integer),
+    schema='changes',
 )
